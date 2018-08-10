39faf94b3d8bd8ef7d4f37ff606c0bb6504556dc
wb-tjh438466 <wb-tjh438466@alibaba-inc.com>
2018-08-10 13:59:13 +0800

    提交代码

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -1,5 +1,8 @@
 package helloWorld;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: wb-tjh438466
  * @date: 2018/8/8
@@ -8,7 +11,45 @@ package helloWorld;
  */
 public class HelloWorld {
     public static void main(String[] args){
+        /*System.out.println("hello world! 啦啦");
 
-        System.out.println("hello world! 啦啦");
+        // 创建不同类型数组： Integer, Double 和 Character
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+        System.out.println( "整型数组元素为:" );
+        printArray( intArray  ); // 传递一个整型数组
+
+        System.out.println( "\n双精度型数组元素为:" );
+        printArray( doubleArray ); // 传递一个双精度型数组
+
+        System.out.println( "\n字符型数组元素为:" );
+        printArray( charArray ); // 传递一个字符型数组
+
+        int a=1;
+        String b="张三";
+        String obj=getInfo(a,"张三");
+        System.out.println(obj);
+
+        List<String> list =new ArrayList<String>();
+        list.add("1");*/
+
+        System.out.println(1<<30);
+    }
+
+    // 泛型方法 printArray
+    public static < E > void printArray( E[] inputArray )
+    {
+        // 输出数组元素
+        for ( E element : inputArray ){
+            System.out.printf( "%s ", element );
+        }
+        System.out.println();
+    }
+
+    public static < M,Z > Z getInfo(M obj0,Z obj1){
+        System.out.println(obj0+"---"+obj1);
+        return  obj1;
     }
 }
